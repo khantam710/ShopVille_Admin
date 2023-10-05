614eab0606cbc251ae24251e820cea238a1c491b
khantam710 <134850822+khantam710@users.noreply.github.com>
2023-10-05 20:23:08 +0530

    add admin files

---
+++ src/firebase.js
@@ -0,0 +1,41 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCsHGLXVaNtMG1DicGl7-lmVcVcx9HaWGw",
+//   authDomain: "dazzle-a4d27.firebaseapp.com",
+//   projectId: "dazzle-a4d27",
+//   storageBucket: "dazzle-a4d27.appspot.com",
+//   messagingSenderId: "237899290832",
+//   appId: "1:237899290832:web:e7dfbd73ff14f7bd72a6d5"
+// };
+
+// // Initialize Firebase
+// const applic = initializeApp(firebaseConfig);
+
+
+// export default applic;
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRcwaEbmiTrH3p5RVpjroDPzm6R3quGlc",
+  authDomain: "shopville-f14f6.firebaseapp.com",
+  projectId: "shopville-f14f6",
+  storageBucket: "shopville-f14f6.appspot.com",
+  messagingSenderId: "30751953834",
+  appId: "1:30751953834:web:85815e202be7c1bb75d1d3"
+};
+
+// Initialize Firebase
+const applic = initializeApp(firebaseConfig);
+
+export default applic;
